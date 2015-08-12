7df61d9cf095a2934a5822a4b2f4f63eca8c73b8
Alex <aleksandernasr@gmail.com>
2015-08-12 15:24:51 +0100

    added ui-view, configured first angular route and injecting template using script type='text/ng-template'

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
 angular.module('redditClone', ['ui.router'])
+.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'MainCtrl'
+    })
+    .state('posts', {
+      url: '/posts/{id}',
+      templateUrl: '/posts.html',
+      controller: 'PostsCtrl'
+    });
+  $urlRouterProvider.otherwise('home');
+}])
+
+
+
 .factory('posts', [function(){
   var o = {
     posts: []
